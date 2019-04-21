fffadb5a1026ded82dcfcb55d3af7fb03098c802
ericli1717 <47766500+ericli1717@users.noreply.github.com>
2019-04-20 20:12:00 -0400

    Update convert_geo.py

--- playground/Kijiji/convert_geo.py
+++ playground/Kijiji/convert_geo.py
@@ -6,7 +6,7 @@ import geocoder
 #geolocator1 = Nominatim(user_agent="convert_geo.py")
 #file2 = open('data_ready.csv', 'ab')
 #geolocator = GeocodeFarm()
-addresses = pd.read_csv("data_pre_ready_5.csv", sep=",", header=None, encoding='utf-8', engine='python')
+addresses = pd.read_csv("data_pre_ready.csv", sep=",", header=None, encoding='utf-8', engine='python')
 #addresses = addresses.loc[0:1136]
 addresses[15]="Null"
 
@@ -32,7 +32,7 @@ def returnxy(address):
 #for feature in test.geojson['features']:
     #print(feature["properties"]["lat"])
     #print(feature["properties"]["lng"])
-for i in range(1,1277):
+for i in range(1,7380):
     test= returnxy(addresses[13][i])
     addresses[14][i] = test[0]
     addresses[15][i] = test[1]
@@ -40,7 +40,7 @@ for i in range(1,1277):
     print(test[0])
     print(test[1])
 
-addresses.to_csv('data_ready_5.csv',index=False)
+addresses.to_csv('data_ready.csv',index=False)
 
 #file2.close()
 #addresses['long'] = addresses[13].apply(lambda x: x[1])
